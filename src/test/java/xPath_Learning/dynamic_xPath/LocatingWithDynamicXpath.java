0b5c2eee07062877bfac1b7dd51767603d2ab849
MahbubulRishad <mahbubulrishad@gmail.com>
2024-12-30 12:51:01 +0600

    Scrolling to element

--- src/test/java/xPath_Learning/dynamic_xPath/LocatingWithDynamicXpath.java
+++ src/test/java/xPath_Learning/dynamic_xPath/LocatingWithDynamicXpath.java
@@ -21,15 +21,23 @@ public class LocatingWithDynamicXpath {
         driver.manage().window().maximize();
 
 //        finding table element
-       WebElement tableEditBtn = driver.findElement(By.xpath("(//div[contains(@class, 'rt-tr-group') and @role= 'rowgroup'])[2]/div/div[7]/div/span[1]"));
+//       WebElement tableEditBtn = driver.findElement(By.xpath("(//div[contains(@class, 'rt-tr-group') and @role= 'rowgroup'])[2]/div/div[7]/div/span[1]"));
+//
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//
+////        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+////        wait.until(ExpectedConditions.elementToBeClickable(tableEditBtn));
+//
+//
+//        js.executeScript("arguments[0].scrollIntoView(true);", tableEditBtn);
+//        tableEditBtn.click();
 
+
+        WebElement tableElAddBtn = driver.findElement(By.xpath("//button[text() = 'Add']"));
         JavascriptExecutor js = (JavascriptExecutor) driver;
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.elementToBeClickable(tableEditBtn));
+        js.executeScript("arguments[0].scrollIntoView(true);", tableElAddBtn);
 
 
-        js.executeScript("arguments[0].scrollIntoView(true);", tableEditBtn);
-        tableEditBtn.click();
+//        driver.quit();
     }
 }
